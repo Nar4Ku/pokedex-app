414dc86883fd0ecf7821b2c32fd5dfbdec051f7c
Luis Fernando <lfpetreca02@gmail.com>
2020-10-29 18:17:19 -0300

    Auto-generated commit

---
+++ common.ef273b9e06c500636bde.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{kRZh:function(n,t,e){"use strict";e.d(t,"a",(function(){return r}));var o=e("fXoL");let r=(()=>{class n{constructor({nativeElement:n}){n.setAttribute("loading","lazy")}}return n.\u0275fac=function(t){return new(t||n)(o.Mb(o.l))},n.\u0275dir=o.Hb({type:n,selectors:[["","appLazyLoadingImg",""]]}),n})()}}]);
